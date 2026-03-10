7d2abcef3d1c5ec8ad222bc035ef860357fa8ba4
Saurabh Mishra <2bsaurabh@gmail.com>
2026-03-10 23:31:56 +0530

    add indexes

--- backend/files/models.py
+++ backend/files/models.py
@@ -9,7 +9,6 @@ def file_upload_path(instance, filename):
     filename = f"{uuid.uuid4()}.{ext}"
     return os.path.join('uploads', filename)
 
-
 class File(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField(upload_to=file_upload_path)
